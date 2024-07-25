623d9d8833d854a6c6478d913847124211a86b05
Rabia Demir <75272533+rabiademirr@users.noreply.github.com>
2024-07-25 17:39:18 +0300

    basket dto and basket item dto added

---
+++ Services/Basket/TrainingCourses.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TrainingCourses.Services.Basket.Dtos
+{
+    public class BasketDto
+	{
+
+		public string UserId { get; set; }
+
+		public string CouponCode { get; set; }
+
+		public List<BasketItemDto> BasketItems { get; set; }
+
+		public decimal TotalPice {
+			get => BasketItems.Sum(x => x.Price * x.Quantity);
+			 }
+	}
+}
+
---
+++ Services/Basket/TrainingCourses.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TrainingCourses.Services.Basket.Dtos
+{
+	public class BasketItemDto
+	{
+		public int Quantity { get; set; }
+
+		public string CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+}
+
