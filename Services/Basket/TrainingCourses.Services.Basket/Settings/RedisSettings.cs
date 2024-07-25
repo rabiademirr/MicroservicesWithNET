b4574c1d5841309a547da3378f29e0e6c6cafcc8
Rabia Demir <75272533+rabiademirr@users.noreply.github.com>
2024-07-25 22:33:37 +0300

    Redis settings class created. port and host information setted.

---
+++ Services/Basket/TrainingCourses.Services.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TrainingCourses.Services.Basket.Settings
+{
+	public class RedisSettings
+	{
+		public string Host { get; set; }
+
+		public int Port { get; set; }
+
+	}
+}
+
