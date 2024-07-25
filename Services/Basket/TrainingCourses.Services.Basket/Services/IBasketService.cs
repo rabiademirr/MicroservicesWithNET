c72aaee7957aeb5d0fdea0dd5fb548fe79ab2232
Rabia Demir <75272533+rabiademirr@users.noreply.github.com>
2024-07-25 22:44:46 +0300

    basket interface created

---
+++ Services/Basket/TrainingCourses.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TrainingCourses.Services.Basket.Dtos;
+using TrainingCourses.Shared.Dtos;
+
+namespace TrainingCourses.Services.Basket.Services
+{
+	public interface IBasketService
+	{
+		Task<Response<BasketDto>> GetBasket(string userId);
+
+		Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+
+		Task<Response<bool>> DeleteBasket(string userId);
+	}
+}
+
